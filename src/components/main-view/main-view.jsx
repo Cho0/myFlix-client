f0898e6f0101059ed2d68ac4990ac71cd5da4565
Cho0 <choojason7@gmail.com>
2021-11-17 19:16:32 -0800

    3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard} from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,14 +9,21 @@ class MainView extends React.Component {
   constructor(){
     super();
     this.state= {
-      movies: [
-        {_id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', ImagePath: '../movie-view/img/inception.jpg'},
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...'},
-        { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...'}
-      ],
-      }
-    };
+      movies: [],
+    }
+  };
   
+  componentDidMount(){
+    axios.get('https://jasons-myflix.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
 
   setSelectedMovie(newSelectedMovie) {
     this.setState({
@@ -25,8 +34,7 @@ class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
@@ -40,6 +48,10 @@ class MainView extends React.Component {
     );
   }
 }
+
+
+//----------------//
+
 
 
 export default MainView;
