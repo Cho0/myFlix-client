58e2b86f60fc7a0c5e7025cf264fae175238b86e
Cho0 <choojason7@gmail.com>
2021-11-18 17:43:23 -0800

    3.4?

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard} from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 
 class MainView extends React.Component {
 
@@ -10,7 +12,9 @@ class MainView extends React.Component {
     super();
     this.state= {
       movies: [],
-    }
+      selectedMovie: null,
+      user: null
+    };
   };
   
   componentDidMount(){
@@ -25,14 +29,22 @@ class MainView extends React.Component {
       });
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  /*setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
+    });
+  }*/
+
+  onLoggedIn(user) {
+    this.setState({
+      user
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
+
+    if(!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     if (movies.length === 0) return <div className="main-view" />;
 
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import PropTypes from  'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+
+    return (<div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>
+    );
   }
 }
+
+MovieCard.proptypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+  }).isRequired,
+  onMovieCLick: PropTypes.func.isRequired
+};
